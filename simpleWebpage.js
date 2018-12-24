aca764586e7b18d943ec45c680159d83b233dc5d
Angphurba Sherpa <44555127+Angphurba@users.noreply.github.com>
2018-12-23 22:30:38 -0500

    Add files via upload

---
+++ simpleWebpage.js
@@ -0,0 +1,151 @@
+
+
+<html>
+
+<head>
+
+            <meta charset="UTF-8">
+			<title> Gun Violence: How The U.S. Compares With Other Countries </title>
+			
+			<style>
+		    <font url("https://fonts.googleapis.com/css?family=Lato" rel="stylesheet"); 
+			
+			.body{
+			position: fixed;
+			overflow-y:scroll
+			width:100%
+			top: -20px;
+			right: -40px;
+			bottom: -40px;
+			widht: auto;
+			height: auto;
+			background-image: "https://www.thetrace.org/wp-content/uploads/2016/01/shutterstock_245379136-1920x1000-c-top.jpg"
+			back-ground-size; cover;
+			-webkit-filter: blur(0px);
+			}
+			
+			.head{
+			
+			position: absolute;
+			top: calc(50% - 35px);
+			left: calc(50% - 255px);
+			}
+			
+			
+			.header div{
+			float: left;
+			color: #fff;
+			font-family: 'Lato', sans-serif;
+			 font-size: 40px;
+			 font-weight: 200px;
+			}
+			
+			.login{
+			position: absolute;
+			top: calc(50% - 75px);
+			left: calc(50% - 50px);
+			height: 150px;
+			width: 350px;
+			padding: 10px;
+			}
+			
+			.login input {type=text}{
+			widht: 250px;
+			height: 30px;
+			background: transparent;
+			border: 1px solid rgba(255,255,255,0.6);
+			border-radius: 2px;
+			color: #fff;
+			font-family: 'Lato', sans-serif;
+			font-size:16px
+			font-weight: 400;
+			padding: 4px;
+			}
+			
+			.login input {type=passwprd}{
+			widht: 250px;
+			height: 30px;
+			background: transparent;
+			border: 1px solid rgba(255,255,255,0.6);
+			border-radius: 2px;
+			color: #fff;
+			font-family: 'Lato', sans-serif;
+			font-size:16px
+			font-weight: 400;
+			padding: 4px;
+			}
+			
+			.login input [type=button]{
+			widht: 250px;
+			height: 35px;
+			background: #fff;
+			border:1px solid #fff;
+			cursor:pointer;
+			border-radius: 2px;
+			color: #al8d6c;
+			font-family: 'Lato', sans-serif;
+			font-size:16px
+			font-weight: 400;
+			padding: 6px;
+			margin-top: 10px;
+			}
+			.login input [type=button]:hover{
+			opacity:0.8;
+			}
+			.login input [type=button]:active{
+			opacity:0.6;
+			}
+			.login input [type=text]:focus{
+			outline:none;
+			border: 1px solid rgba (255,255,255,0.9);
+			}
+			.login input [type=password]:focus{
+			outline:none;
+			border:1px solid rgba (255,255,255,0.9);
+			}
+			.login input [type=button]:focus{
+			outline:none;
+			}
+			::-webkit-imput-placeholder{
+			color: rgba(255,255,255,0.6);
+			}
+			::-moz-input-placeholder{
+			color: rgba(255,255,255,0.6);}
+			
+			</body>
+			
+			</style>
+			
+			
+</head>
+
+
+<body oncontextmenu="return false">
+<div class="body"></div>
+<div class="grad"></div>
+<div class="header">
+<div>Please<span>subscribe</span></div>
+</div>
+<br>
+<form name="login">
+<div class="login">
+     <input type="text" placeholder="username" name="userid"><br>
+	 <input type="password: placeholder="password" name="pswrd"><br>
+	 <input type="button" onclick="check(this.form)" value="Login"/>
+	 </div>
+	 </form>
+	 
+	 <script language="javascript">
+	 fucntion check(form)
+	 {
+	 if(form.userid.value=="" && form.pswrd.value =="")
+	 {
+	 window.open(http://www.npr.org/sections/goatsandsoda/2017/10/06/555861898/gun-violence-how-the-u-s-compares-to-other-countries)
+	 }
+	 else()
+	 }
+	 </body>
+	 
+</html>
+	 
+	 
